8974701ad8b5cd38116f608ebfe9407928b1cdaa
Vasyl Diachuk <djachukvasja@gmail.com>
2024-04-11 18:23:11 +0300

    2

--- src/shared/components/ModalBackdrop/ModalBackdrop.jsx
+++ src/shared/components/ModalBackdrop/ModalBackdrop.jsx
@@ -57,7 +57,7 @@ const ModalBackdrop = ({ children, onClose }) => {
     <div
       className={dinamicStyle}
       onClick={handleCloseModal}
-      style={isMobile && { top: '10px', height: `${h1 - 20}px` }}
+      style={{ top: '10px', height: `${h1 - 20}px` }}
     >
       <div style={{ color: 'red' }}>
         h1 {h1}
